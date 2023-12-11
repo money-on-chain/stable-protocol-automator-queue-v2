0ff822a594a4b8b3566efb237b88559c20f1e9c9
mulonemartin <mulone.martin@gmail.com>
2023-12-11 15:54:30 -0300

    init version

---
+++ automator/tasks.py
@@ -0,0 +1,140 @@
+import decimal
+from web3 import Web3
+import datetime
+
+from .contracts import Multicall2, MocQueue
+
+from .base.main import ConnectionHelperBase
+from .tasks_manager import PendingTransactionsTasksManager, on_pending_transactions
+from .logger import log
+from .utils import aws_put_metric_heart_beat
+
+
+__VERSION__ = '1.0.0'
+
+
+log.info("Starting Stable Protocol Queue Automator version {0}".format(__VERSION__))
+
+
+class Automator(PendingTransactionsTasksManager):
+
+    def __init__(self,
+                 config,
+                 connection_helper,
+                 contracts_loaded
+                 ):
+        self.config = config
+        self.connection_helper = connection_helper
+        self.contracts_loaded = contracts_loaded
+
+        # init PendingTransactionsTasksManager
+        super().__init__(self.config,
+                         self.connection_helper,
+                         self.contracts_loaded)
+
+    @on_pending_transactions
+    def execute(self, task=None, global_manager=None, task_result=None):
+
+        # If ready to execute the queue?
+        ready_to_execute = self.contracts_loaded["MocQueue"].sc.functions.readyToExecute().call()
+        if ready_to_execute:
+
+            # return if there are pending transactions
+            if task_result.get('pending_transactions', None):
+                return task_result
+
+            web3 = self.connection_helper.connection_manager.web3
+
+            nonce = web3.eth.get_transaction_count(
+                self.connection_helper.connection_manager.accounts[0].address, "pending")
+
+            # get gas price from node
+            node_gas_price = decimal.Decimal(Web3.from_wei(web3.eth.gas_price, 'ether'))
+
+            # Multiply factor of the using gas price
+            calculated_gas_price = node_gas_price * decimal.Decimal(self.config['gas_price_multiply_factor'])
+
+            try:
+                tx_hash = self.contracts_loaded["MocQueue"].execute(
+                    self.config['tasks']['execute']['fee_recipient'],
+                    gas_limit=self.config['tasks']['execute']['gas_limit'],
+                    gas_price=int(calculated_gas_price * 10 ** 18),
+                    nonce=nonce
+                )
+            except ValueError as err:
+                log.error("Task :: {0} :: Error sending transaction! \n {1}".format(task.task_name, err))
+                return task_result
+
+            if tx_hash:
+                new_tx = dict()
+                new_tx['hash'] = tx_hash
+                new_tx['timestamp'] = datetime.datetime.now()
+                new_tx['gas_price'] = calculated_gas_price
+                new_tx['nonce'] = nonce
+                new_tx['timeout'] = self.config['tasks']['execute']['wait_timeout']
+                task_result['pending_transactions'].append(new_tx)
+
+                log.info("Task :: {0} :: Sending TX :: Hash: [{1}] Nonce: [{2}] Gas Price: [{3}]".format(
+                    task.task_name, Web3.to_hex(new_tx['hash']), new_tx['nonce'], int(calculated_gas_price * 10 ** 18)))
+
+        else:
+            log.info("Task :: {0} :: No!".format(task.task_name))
+
+        return task_result
+
+
+class AutomatorTasks(Automator):
+
+    def __init__(self, config):
+
+        self.config = config
+        self.connection_helper = ConnectionHelperBase(config)
+
+        self.contracts_loaded = dict()
+        self.contracts_addresses = dict()
+
+        # contract addresses
+        self.load_contracts()
+
+        # init automator
+        super().__init__(self.config,
+                         self.connection_helper,
+                         self.contracts_loaded)
+
+        # Add tasks
+        self.schedule_tasks()
+
+    def load_contracts(self):
+        """ Get contract address to use later """
+
+        log.info("Getting addresses from Main Contract...")
+
+        # Moc
+        self.contracts_loaded["MocQueue"] = MocQueue(
+            self.connection_helper.connection_manager,
+            contract_address=self.config['addresses']['MocQueue'])
+        self.contracts_addresses['MocQueue'] = self.contracts_loaded["MocQueue"].address().lower()
+
+        # Multicall
+        self.contracts_loaded["Multicall2"] = Multicall2(
+            self.connection_helper.connection_manager,
+            contract_address=self.config['addresses']['Multicall2'])
+
+    def schedule_tasks(self):
+
+        log.info("Starting adding tasks...")
+
+        # set max workers
+        self.max_workers = 1
+
+        if 'execute' in self.config['tasks']:
+            log.info("Jobs add: 1. Execute Queue")
+            interval = self.config['tasks']['execute']['interval']
+            self.add_task(self.execute,
+                          args=[],
+                          wait=interval,
+                          timeout=180,
+                          task_name='1. Execute Queue')
+
+        # Set max workers
+        self.max_tasks = len(self.tasks)
---
+++ docker_build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# exit as soon as an error happen
+set -e
+
+usage() { echo "Usage: $0 -e <environment> -c <config file> -i <aws id>" 1>&2; exit 1; }
+
+while getopts ":e:c:i:" o; do
+    case "${o}" in
+        e)
+            e=${OPTARG}
+             ((e == "flipago_testnet"  || e=="flipago_mainnet" || e == "roc_testnet" || e == "roc_mainnet")) || usage
+            case $e in
+                flipago_testnet)
+                    ENV=$e
+                    ;;
+                flipago_mainnet)
+                    ENV=$e
+                    ;;
+                roc_testnet)
+                    ENV=$e
+                    ;;
+                roc_mainnet)
+                    ENV=$e
+                    ;;
+                *)
+                    usage
+                    ;;
+            esac
+            ;;
+        c)
+            c=${OPTARG}
+            CONFIG_FILE=$c
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${e}" ] || [ -z "${c}" ]; then
+    usage
+fi
+
+docker image build -t automator_queue_$ENV -f Dockerfile --build-arg CONFIG=$CONFIG_FILE .
+
+echo "Build done!"
